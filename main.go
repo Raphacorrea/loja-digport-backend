a6306d3c809969a18be875e00a632dafc9804351
RaphaCorrea <raphaellemontano@hotmail.com>
2024-08-30 16:29:44 -0300

    new classes added

--- main.go
+++ main.go
@@ -10,21 +10,21 @@ func main() {
 	fmt.Println("Hello, world!")
 
 	ListaDeDesejos := model.Carrinho{
-		ID:        "ID do carrinho",
-		UserID:    "ID do Usuario",
+		ID:     "ID do carrinho",
+		UserID: "ID do Usuario",
 		InfosProduto: []model.InfosProduto{
-		
-		ProdutoID: "produto1",
-		QuantidadeDeProduto: 3,
 
-	},
-	{
-		ProdutoID: "Produto2",
-		QuantidadeDeProduto: 1,
-	},
-	//fmt.Printf(`%+v`, ListaDeDesejos)
+			ProdutoID:           "produto1",
+			QuantidadeDeProduto: 3,
+		},
+		{
+			ProdutoID:           "Produto2",
+			QuantidadeDeProduto: 1,
+		},
+		//fmt.Printf(`%+v`, ListaDeDesejos)
+	}
 }
+
 //InfosProduto: map[string]int{
 //"id-blusa-roxa:1,"
 //""id-blusa-rosa:2
-//}
